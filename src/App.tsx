bf4df8ef050fe46bba5a805db7ac9d388be9b115
misakisoeda <misaki.soeda.29@gmail.com>
2023-04-24 22:12:45 +0900

    Function Components 入門編

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+interface AppProps {
+  message: string;
+}
+
+// const App = ({ message }: { message: string }) => {  //→再利用できないので、interfaceを作成して、下記で対応
+const App = ({ message }: AppProps) => {
+  return <div>{ message }</div>;
 };
 
 export default App;
